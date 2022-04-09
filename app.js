37822278a07295ed673c01b3cc6b6b2368490e85
Francisco Moreno <franciscoj.sparcoltary>
2022-04-09 18:04:42 -0500

    (feature): add fireworks

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const dateTarget = new Date('04/13/2022 06:00 AM');
+const dateTarget = new Date('04/13/2022 6:00 AM');
 const remainingTime = document.querySelector('#remainingTime');
 const milisecondsOfASecond = 1000;
 const milisecondsOfAMinute = milisecondsOfASecond * 60;
 const milisecondsOfAHour = milisecondsOfAMinute * 60;
 const milisecondsOfADay = milisecondsOfAHour * 24;
+const fireworks = document.querySelector('#fireworks');
 const colors = [
-   '#d39af3',
-   '#d39af3',
-   '#BC96E6',
-   '#d39af3',
-   '#d39af3',
-   '#BC96E6',
-   '#d39af3',
-   '#d39af3',
-   '#BC96E6',
-   '#d39af3',
+   '#d39af3'
 ];
 
 function changeBackground(color) {
     document.body.style.background = color;
+}
+
+function hideCounter() {
+    remainingTime.textContent = 'Disfruta tu vuelo, al rato te veo';
+}
+
+function launchFireworks() {
+    fireworks.style.display = 'initial';
+    hideCounter();
 }
 
 function updateCountdown() {
@@ -28,11 +29,15 @@ function updateCountdown() {
     const remainingsHours = Math.floor((duration % milisecondsOfADay) / milisecondsOfAHour);
     const remainingMinutes = Math.floor((duration % milisecondsOfAHour) / milisecondsOfAMinute);
     const remainingSeconds = Math.floor((duration % milisecondsOfAMinute) / milisecondsOfASecond);
+    const remainingNumber = remainingsDays.toString() +  remainingsHours.toString() + remainingMinutes.toString() + remainingSeconds.toString();
 
     remainingTime.textContent = `${remainingsDays} dias, ${remainingsHours} : ${remainingMinutes} : ${remainingSeconds}`
 
-    const index = parseInt(remainingSeconds.toString().slice(-1))
-    changeBackground(colors[index]);
+    const remainingNumberParsed = parseInt(remainingNumber);
+    if(remainingNumberParsed <= 0) {
+        launchFireworks();
+    }
+    changeBackground(colors[0]);
 }
 
 updateCountdown();
